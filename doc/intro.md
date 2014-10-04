4ba1de34b81224db7d368281de55111f4291e7ae
Christopher Small <metasoarous@gmail.com>
2014-10-04 12:48:25 -0700

    Initialized empty project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to chicken-coop
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/chicken_coop/core_test.clj
@@ -0,0 +1,7 @@
+(ns chicken-coop.core-test
+  (:require [clojure.test :refer :all]
+            [chicken-coop.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
