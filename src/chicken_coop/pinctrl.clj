890482b7a9d5398b10b7cea3aa26b76cb91733dc
Christopher Small <metasoarous@gmail.com>
2014-10-06 09:48:51 -0700

    Have gpio working\!

--- src/chicken_coop/pinctrl.clj
+++ src/chicken_coop/pinctrl.clj
@@ -1,6 +1,7 @@
 (ns chicken-coop.pinctrl
   (:require [clojure.java.io :as io]
-            [clojure.data.csv :as csv]))
+            [clojure.data.csv :as csv]
+            [clojure.tools.trace :as tr]))
 
 
 (def gpio-path "/sys/class/gpio")
@@ -13,23 +14,31 @@
       x)))
 
 
+(defn -read-csv-row [row]
+  (first (csv/read-csv row)))
+
+
 (defn read-pinout-spec [pinout-fn]
+  (println "about to read pinout spec")
   (with-open [f (io/reader pinout-fn)]
-    (let [non-cmt-lines (remove #(or (re-find #"^\#" %)
-                                     (re-find "^\s*$" %))
-                                (line-seq f))
-          header (csv/read-csv (first non-cmt-lines))
-          non-cmnt-lines (rest non-cmt-lines)]
-      ; The key of the outer most hash: lets us find configs by (eg) (pinout :P8 9)
+    (let [non-cmnt-lines (remove #(or (re-find #"^\#" %)
+                                      (re-find #"^\s*$" %))
+                                 (line-seq f))
+          header (-read-csv-row (first non-cmnt-lines))
+          non-cmnt-lines (rest non-cmnt-lines)]
+      ; Build up a hash mapping header, pin-n pairs to data about the pin
       (into
         {}
-        (for [l non-cmt-lines]
-          [[(keyword (non-cmt-lines 0))
-            (-safe-int (non-cmt-lines 1))]
-           (into {}
-             (map vector header (csv/read-csv non-cmt-lines)))])))))
+        (for [line-str non-cmnt-lines]
+          (let [line-vec (-read-csv-row line-str)]
+            ; the key; eg [:P8 34], for header and pin
+            [[(keyword (first line-vec))
+              (-safe-int (second line-vec))]
+             ; the value; a map of key, value pairs for the corresponding header, pin pair
+             (into {}
+               (map #(vector (keyword %1) %2) header line-vec))]))))))
 
-(def pinout-spec* (memoize read-pinout-spec))
+(def pinout-spec* (memoize (partial read-pinout-spec "etc/pinout.csv")))
 
 
 (defn write-bytes [filename b]
@@ -46,58 +55,84 @@
 
 (defn get-gpio-n [header pin]
   (->
-    (pinout-spec* "etc/pinout.csv")
-    (apply [header pin])
+    (pinout-spec*)
+    (apply [[header pin]])
     :name
-    (re-matcher #"GPIO(\d)_(\d*)")
-    (->> (map #(Integer/parseInt %)))
-    (as-> m (+ (* 32 (m 1))
-               (m 2)))
+    (->> (re-matches #"GPIO(\d)_(\d*)")
+         (rest)
+         (map #(Integer/parseInt %)))
+    (as-> m (+ (* 32 (first m))
+               (second m)))
     (str)))
 
 
-(defprotocol BBBPin
-  (init! [p])
-  (write! [p value])
-  (read! [p]))
+(defn kw->str [kw]
+  (clojure.string/replace (str kw) ":" ""))
 
-(defrecord GPIO [header pin mode]
+
+(defprotocol BBBPin
+  (init! [this]
+    "Initialize pin")
+  (write! [this value]
+    "Write to pin (:on or :off for gpio)")
+  (read! [this]
+    "Read from pin")
+  (close! [this]
+    "Close pin for operation (cleanup)"))
+
+(defrecord GPIO [header pin direction]
   BBBPin
-  (init! [_]
+  (init! [this]
     ; should set :pre checks here?
     (let [pin-n (get-gpio-n header pin)]
+      (assoc this :pin-n pin-n)
+      ; Activate the pin
       (write-bytes "/sys/class/gpio/export" pin-n)
-      (write-bytes (str "/sys/class/gpio" pin-n "/direction") mode)))
-  (write!
-    "Specify value as on or off (as str or kw) or 1 or 0 (as str or int)"
-    [_ value]
-    {:pre [(#{:on :off} mode)]}
+      (try
+        (Thread/sleep 1000) ; Actually takes a sec for the control files to init...
+        ; Set the direction
+        (write-bytes (str "/sys/class/gpio/gpio" pin-n "/direction") (kw->str direction))
+        ; In case anything goes wrong with that, try to close the gpio
+        (catch Exception e
+          (close! this)
+          (throw e)))))
+  (write! [_ value]
+    {:pre [(#{:on :off} direction)]}
     (write-bytes
-      (str "/sys/class/gpio" (get-gpio-n header pin) "/value")
+      (str "/sys/class/gpio/gpio" (get-gpio-n header pin) "/value")
       ({:on "1" :off "0"} value)))
   (read! [_]
-    ({"1" :on "0" :off} (read-bytes (str "/sys/class/gpio" (get-gpio-n header pin) "/value")))))
+    ({"1" :on "0" :off}
+     (read-bytes
+       (str "/sys/class/gpio/gpio" (get-gpio-n header pin) "/value"))))
+  (close! [_]
+    (write-bytes
+      (str "/sys/class/gpio/unexport")
+      (get-gpio-n header pin))))
 
 
-(defn gpio [header pin mode]
+(defn gpio [header pin direction]
   {:pre [(integer? pin) ; should make sure valid pin
-         (#{"P8" "P9"} header)
-         (#{"in" "out" "high" "low"} mode)]}
-  (let [g (GPIO. pin mode)]
+         (#{:P8 :P9} header)
+         (#{:in :out :high :low} direction)]}
+  (let [g (GPIO. header pin direction)]
     (init! g)
     g))
 
 
 (defn -main []
   (println "ready to initialize")
-  (let [g (gpio :P8 3 :out)]
-    (println "initialized successfully")
-    (doseq [_ (range)]
-      (println "Writing :on")
-      (write! g :on)
-      (Thread/sleep 1000)
-      (println "Writing :off")
-      (write! g :off)
-      (Thread/sleep 1000))))
+  (let [g (gpio :P8 11 :low)]
+    (try
+      (println "Pin settings" ((pinout-spec*) [:P8 11]))
+      (println "initialized successfully")
+      (doseq [_ (range 100)]
+        (println "Writing :on")
+        (write! g :on)
+        (Thread/sleep 1000)
+        (println "Writing :off")
+        (write! g :off)
+        (Thread/sleep 1000))
+      (finally (close! g)))))
 
 
