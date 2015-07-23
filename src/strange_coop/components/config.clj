dc4ecf5601e7651b282fe3fe8948808481d28990
Christopher Small <metasoarous@gmail.com>
2015-07-22 21:52:09 -0700

    First pass at config component

---
+++ src/strange_coop/components/config.clj
@@ -0,0 +1,91 @@
+(ns strange-coop.components.config
+  (:require [com.stuartsierra.component :as component]
+            [environ.core :refer [env]]
+            [strange-coop.util :as util :refer [log]]))
+
+(defn deep-merge
+  "Like merge, but merges maps recursively."
+  [& maps]
+  (if (every? map? maps)
+    (apply merge-with deep-merge maps)
+    (last maps)))
+
+(defn deep-merge-in
+  [m path & maps]
+  (apply update-in m path deep-merge maps))
+
+(def defaults
+  {:nrepl-port 9999
+   :satellite {:url "http://localhost:3000/"}
+   :channels {:buffer-size 100}
+   :day-state-machine {:day-threshold   0.13
+                       :night-threshold 0.03}
+   :pins  {:motor-hb     {:pins   [16 17 18]
+                          :header :P8}}
+           :light-sensor [:P9 33]
+           :temp-sensor  [:P9 35]
+           :floor-button [:P8 11 :normally-off]
+           :roof-button  [:P8 12 :normally-on]})
+
+(defn ->long [x]
+  (Long/parseLong x))
+
+(def rules
+  "Mapping of env keys to parsing options"
+  {:nrepl-port         {:parse ->long}
+   :satellite-url      {:path [:satellite :url]}
+   :satellite-username {:path [:satellite :username]}
+   :satellite-password {:path [:satellite :password]}
+   })
+
+(defn get-environ-config [rules env]
+  (reduce
+    (fn [config [name {:keys [parse path]}]]
+      (assoc-in config (or path [name]) ((or parse identity) (get env name))))
+    {}
+    rules))
+
+(defrecord Config [overrides config]
+  component/Lifecycle
+  (start [component]
+    (log "Starting config component")
+    (deep-merge-in component [:config]
+                   defaults
+                   (get-environ-config rules env)
+                   overrides))
+  (stop [component]
+    (log "Stopping config component")
+    (dissoc component :config)))
+ 
+(defn new-config
+  [overrides]
+  (map->Config {:overrides overrides}))
+
+(defn get-in-config
+  "Little helper for making getting config variables less awkward"
+  ([component path]
+   (get-in-config component path nil))
+  ([component path default]
+   (get-in component (into [:config :config] path) default)))
+
+
+;;; Sketches
+;;; ========
+
+;; Maybe we should just scrap having a separate level for :config... ???
+
+;(defn make-rule
+  ;([name]
+   ;{:name name :path name})
+  ;([name k v & {:as kw-args}]
+   ;(merge (make-rule name) {k v} kw-args)))
+;(def rule make-rule)
+;(defmacro with-merges
+  ;[{:keys [subpath parse]}
+     ;(rule :motor-hb-n-pins
+           ;:path [:motor-hb :n-pins]
+           ;:parse ->long)
+     ;(rule :motor-hb-power-pin
+           ;:path [:motor-hb :power-pin]
+           ;:parse ->long)
+
