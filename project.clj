fe21d715510e57666a2bc835e8256884e25c20c1
Christopher Small <metasoarous@gmail.com>
2015-08-15 08:21:36 -0700

    Faulty project.clj

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.csv "0.1.2"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/tools.logging "0.3.1"]
-  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.trace "0.7.8"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [com.stuartsierra/component "0.2.3"]
-                 ; For when we set up websockets
                  [stylefruits/gniazdo "0.4.0"]
+                 [base64-clj "0.1.1"]
                  [environ "1.0.0"]
                  [cheshire "5.5.0"]
                  [http-kit "2.1.18"]
