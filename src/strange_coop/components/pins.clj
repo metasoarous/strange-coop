bc365759b6b36b5f84fb0afd516494b99c1c7a9f
Christopher T Small <metasoarous@gmail.com>
2015-08-23 08:21:30 +0700

    Setting up mock config variable in pins component

--- src/strange_coop/components/pins.clj
+++ src/strange_coop/components/pins.clj
@@ -2,6 +2,7 @@
   (:require [clojure.core.async :as async :refer [chan <!! >!! go go-loop >! <! close!]]
             [com.stuartsierra.component :as component]
             [strange-coop.bbbpin :as bb]
+            [strange-coop.components.config :as config]
             [strange-coop.button :as button]
             [strange-coop.hbridge :as hb]))
 
@@ -11,13 +12,14 @@
 (defrecord Pins [config]
   component/Lifecycle
   (start [component]
-    (assoc component
-           :floor-button  (button/button :P8 11 :normally-off)
-           :roof-button   (button/button :P8 12 :normally-on)
-           :light-sensor  (bb/ain 33)
-           :temp-sensor   (bb/ain 35)
-           :motor-control (hb/hbridge [16 17 18] :header :P8)
-           :status-led    (bb/gpio :P8 14 :out)))
+    (with-bindings {#'bb/*mock?* (config/get-in-config component [:mock])}
+      (assoc component
+             :floor-button  (button/button :P8 11 :normally-off)
+             :roof-button   (button/button :P8 12 :normally-on)
+             :light-sensor  (bb/ain 33)
+             :temp-sensor   (bb/ain 35)
+             :motor-control (hb/hbridge [16 17 18] :header :P8)
+             :status-led    (bb/gpio :P8 14 :out))))
 
   (stop [component]
     (apply dissoc component pin-keys)))
